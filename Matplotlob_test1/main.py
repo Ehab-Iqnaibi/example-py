15cd0d9e1138781122baaa2182b1019d47bb3b30
Ehab-Iqnaibi <iqnaibi_ehab@ppu.edu>
2022-12-27 13:27:42 +0200

    ehab27/12

--- Matplotlob_test1/main.py
+++ Matplotlob_test1/main.py
@@ -30,8 +30,8 @@ ax.set(xlabel='Days', ylabel='Temperature',
 ax.plot(days, temperature,
           days, temperature, "or")
 plt.show()
-'''
-# np with matplot
+
+#4 np with matplot
 # 100 point start from -2pi to 2pi
 x = np.linspace(-2 * np.pi, 2 * np.pi, 100)
 f_sin = 3*np.sin(x)
@@ -44,4 +44,22 @@ ax.axis([startx, endx, starty, endy])
 
 ax.set(xlabel='x-axis',ylabel='y-axis',title='Some periodic functions')
 ax.plot(x, f_sin, x, f_2)
+plt.show()
+'''
+#4 spines,ticks,
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.linspace(-2 * np.pi, 2 * np.pi, 100)
+f_sin = 3 * np.sin(x)
+f_2 = np.sin(2 * x) - np.cos(x)
+fig, ax = plt.subplots()
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['bottom'].set_position(('data', 0))
+ax.spines['left'].set_position(('data', 0))
+ax.set_xticks([-2.0 * np.pi, -1.5 * np.pi, -1.0 * np.pi, -0.5 * np.pi,
+                0.5 * np.pi, 1.0 * np.pi, 1.5 * np.pi, 2.0 * np.pi])
+ax.set_yticks([-3, -1, +1, 3])
+ax.set_title('Some periodic functions')
+ax.plot(x, f_sin, '--', x, f_2, 'r')
 plt.show()
